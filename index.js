ba1a146ad097093616b88091824b24ea2f91b49b
Trevor Landau <landautrevor@gmail.com>
2015-11-26 09:25:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // Converts a buffer to a string
 // if `buf` is not a buffer it will turn it into one
-// so it better be something Buffer likes, dawg.
+// so it better be something Buffer likes.
 var api = module.exports = function buf2str(buf, encoding) {
   encoding = encoding || api.defaultEncoding;
 
